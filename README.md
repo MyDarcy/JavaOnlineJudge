282761845933d27eb68742b31efda154ad993720
darcy he <darcy.q.cs@gmail.com>
2017-04-05 21:02:22 +0800

    init add commit first time

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# 应该会包括几个部分
+## 剑指offer的题目
+- a 
+- b
+- b
+
+## leetcode 的题目;
+- a
+- b
+- c
+
+## nowcoder上面的一些题目
+- a
+- b
+- c
+
+----
+
+# to be continue...
---
+++ src/com/darcy/main/Main.java
@@ -0,0 +1,12 @@
+package com.darcy.main;
+
+/**
+ * Author by darcy
+ * Date on 17-4-5 下午8:33.
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("just for test.");
+    }
+}
