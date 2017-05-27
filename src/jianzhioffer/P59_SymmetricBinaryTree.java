19d6e82828752baafe743bdc24445d0b5edc389b
MyDarcy <darcy.q.cs@gmail.com>
2017-05-28 00:48:06 +0800

    to P60

--- src/jianzhioffer/P59_SymmetricBinaryTree.java
+++ src/jianzhioffer/P59_SymmetricBinaryTree.java
@@ -21,10 +21,60 @@ public class P59_SymmetricBinaryTree {
 
     }
 
+    /**
+     * 比较二叉树的前序遍历序列和对称前序遍历序列来断定二叉树是否是对称的。
+     *
+     * @param pRoot
+     * @return
+     */
     boolean isSymmetrical(TreeNode pRoot) {
-
-        return false;
-
+        return isSymmetrical(pRoot, pRoot);
     }
+
+    private boolean isSymmetrical(TreeNode root1, TreeNode root2) {
+        // 这样的判定就能去掉三种情况;
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        // left -> right,  right -> left. 肯定可以保证节点的个数是相等的，如果是对称的话。
+        return isSymmetrical(root1.left, root2.right)
+                && isSymmetrical(root1.right, root2.left);
+    }
+
+
+    /*
+     * 链接：https://www.nowcoder.com/questionTerminal/ff05d44dfdb04e1d83bdbdab320efbcb
+     */
+
+    boolean isSymmetrical2(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+        return comRoot(pRoot.left, pRoot.right);
+    }
+
+    private boolean comRoot(TreeNode left, TreeNode right) {
+// TODO Auto-generated method stub
+        if (left == null) return right == null;
+        if (right == null) return false;
+        if (left.val != right.val) return false;
+        return comRoot(left.right, right.left) && comRoot(left.left, right.right);
+    }
+
+
+    /*
+     * C++ 迭代版本的实现
+       链接：https://www.nowcoder.com/questionTerminal/ff05d44dfdb04e1d83bdbdab320efbcb
+     */
+
 
 }
---
+++ src/jianzhioffer/P60_PrintTreeInLevel.java
@@ -0,0 +1,104 @@
+package jianzhioffer;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Created by darcy
+ * 2017/5/28--0:24
+ * Description:
+ * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+ */
+public class P60_PrintTreeInLevel {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    /**
+     * 因为是整数，所以以-1作为结束节点并不具有通用性。
+     * @param pRoot
+     * @return
+     */
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        if (pRoot == null) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        Deque<TreeNode> queue1 = new LinkedList<>();
+
+
+        queue1.addLast(pRoot);
+
+        while (!queue1.isEmpty()) {
+            ArrayList<Integer> tempList = new ArrayList<>();
+            Deque<TreeNode> queue2 = new LinkedList<>();
+            for (TreeNode node : queue1) {
+                tempList.add(node.val);
+                if (node.left != null) {
+                    queue2.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue2.addLast(node.right);
+                }
+            }
+            result.add(tempList);
+            // may lead to GC.
+            queue1 = null;
+            queue1 = queue2;
+        }
+
+        return result;
+
+    }
+
+    /**
+     * 这种方式报 java.util.ConcurrentModificationException
+     * @param pRoot
+     * @return
+     */
+    ArrayList<ArrayList<Integer>> Print2(TreeNode pRoot) {
+        if (pRoot == null) {
+            return new ArrayList<>();
+        }
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        Deque<TreeNode> queue1 = new LinkedList<>();
+        Deque<TreeNode> queue2 = new LinkedList<>();
+
+
+        queue1.addLast(pRoot);
+
+        while (!queue1.isEmpty()) {
+            ArrayList<Integer> tempList = new ArrayList<>();
+            for (TreeNode node : queue1) {
+                tempList.add(node.val);
+                if (node.left != null) {
+                    queue2.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue2.addLast(node.right);
+                }
+            }
+            result.add(tempList);
+            queue1.clear();
+            queue1 = queue2;
+            queue2 = queue1;
+        }
+
+        return result;
+
+    }
+}
