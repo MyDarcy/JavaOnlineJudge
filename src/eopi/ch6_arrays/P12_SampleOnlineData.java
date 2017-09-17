c01aaef49ee847e764e92ea1111704d4f774ac3b
darcy he <darcy.q.cs@gmail.com>
2017-09-17 14:50:04 +0800

    eopi array add permutations.

---
+++ src/eopi/ch6_arrays/P12_SampleOnlineData.java
@@ -0,0 +1,91 @@
+package eopi.ch6_arrays;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Author by darcy
+ * Date on 17-9-17 下午12:53.
+ * Description:
+ *
+ * 连续读取的n个数字中随机均匀的选择K个数字.
+ *
+ * Design a program that takes as input a size k,and reads packets,
+ * continuously maintaining a uniform random subset of size k of the read packets.
+ *
+ * Hint: Suppose you have a procedure which selects k packets from the first
+ * n > k packets asspecified. How would you deal with the (n + l)th packet?
+ */
+public class P12_SampleOnlineData {
+
+  /**
+   * 暴力破解算法, 缓存读取的n个字符串.然后选择k个.借用上一个算法的方式.
+   * <p>
+   * 空间复杂度O(n),时间复杂度O(nk)
+   *
+   * @param iterator
+   */
+  public static List<Integer> bruteForceSolution(Iterator<Integer> iterator, int k) {
+    return null;
+  }
+
+  /**
+   *  读到一个新的数字, 那么它被选中作为候选集的元素的概率是 k / currentNumber;
+   *  这里采用了一种巧妙的想法:就是当index = random.nextInt(currentNumber) < k的时候,
+   *  那么就将读取到的数字替换index处的元素.这就是被选择的概率为k/currentNumber的体现.
+   *
+   * 时间复杂度O(N),空间复杂度O(k)
+   * 问题: 单词迭代之间并不是彼此独立的,一次迭代的后继最多只有一个元素和当前迭代不同.
+   *
+   *  As an example, suppose k - 2, and the packets are read in the order p,q,r,t,u,v.
+   *  We keep the first two packets in the subset, which is {p,q}. We select the next packet, r,
+   *  with probability 2/3. Suppose it is not selected. Then the subset after reading the first
+   *  three packets is still {p,q}. We select the next packet, t, with probability 2/4. Suppose
+   *  it is selected. Then we choose one of the packets in {p,q) uniformly, and replace it
+   *  with t. Let q be the selected packet—now the subset is [p,t}. We select the next packetu
+   *  with probability 2/5. Suppose it is selected. Then we choose one of the packets in
+   *  {p,t} uniformly, and replace it with u. Let t be the selected packet—now the subset is
+   *  {p,u}. We select the next packet v with probability 2/6. Suppose it is not selected. The
+   *  random subset remains {p,u}
+   * @param iterator
+   * @return
+   */
+  public static List<Integer> solution(Iterator<Integer> iterator, int k) {
+    List<Integer> result = new ArrayList<>(k);
+    // 先读取k个.
+    for (int i = 0; i < k && iterator.hasNext(); i++) {
+      result.add(iterator.next());
+    }
+
+    int numberSoFar = k;
+    Random randomForNextIndex = new Random();
+    while (iterator.hasNext()) {
+      Integer element = iterator.next();
+      numberSoFar++;
+      // 有 k / numberSoFar的概率使得index落在[0, k]之间,这就代表了可以将当前读取的数字移动到index位置.
+      int indexToReplace = randomForNextIndex.nextInt(numberSoFar);
+      if (indexToReplace < k) {
+        result.set(indexToReplace, element);
+      }
+      // 结果集合:
+      System.out.println(result);
+    }
+
+    return result;
+  }
+
+
+  public static void main(String[] args) {
+    int number = 100;
+    List<Integer> list = new ArrayList<>(number);
+    for (int i = 0; i < number; i++) {
+      list.add(i);
+    }
+
+    Iterator<Integer> iterator = list.iterator();
+    List<Integer> result = solution(iterator, 5);
+    System.out.println(result);
+  }
+}
---
+++ src/eopi/ch6_arrays/P13_ComputeARandomPermutation.java
@@ -0,0 +1,84 @@
+package eopi.ch6_arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Author by darcy
+ * Date on 17-9-17 下午2:10.
+ * Description:
+ *
+ * 设计一个算法能改生成{0,1,...n-1}的随机均匀的permutation.所有可能的permutation都有相同的概率.
+ *
+ * Design an algorithm that creates uniformly random permutations of {0,1,... n-1}.You are
+ * given a random number generator that returns integers in the set {0,1,...,n-1}
+ * with equal probability; use as few calls to it as possible.
+ *
+ * Hint: If the result is stored in A,how would you proceed once A[n-1] is assigned correctly?
+ *
+ */
+public class P13_ComputeARandomPermutation {
+
+  /**
+   * 迭代的选择[0, n-1]之间的数, 重复的话就再次选择,直到选择到了n个数字. 使用HashMap来表示number是否被选择过.
+   *
+   * For example, if n = 4, we might have the sequence 1, 2,1 (repeats), 3,1(repeats), 2
+   * (repeat),0 (done, all numbersfrom 0 to 3 are present). The corresponding permutation
+   * is (1,2,3,0).
+   *
+   * 空间复杂度O(n),时间复杂度O(nlogn)
+   *
+   *
+   * @param n
+   * @return
+   */
+  public static List<Integer> bruteForceSolution(int n) {
+
+    return null;
+  }
+
+
+  /**
+   * 因为暴力的方案里面存储大量的重复选择,所以最终的时间复杂度也上来了.是O(nlogn)
+   *
+   * 采用swap的机制. 后面randdom.nextInt(given.size() - i) + i可以生成的index的个数分别是
+   * n, n-1, n-2, n-3, n-4....(i = 0,1,...n - 1);这就相当于是 n! 的各个部分.
+   *
+   * 时间复杂度O(n),空间复杂度(没有额外的)
+   *
+   * If we apply P11_SampleOfflineDate to (0,1,2,...,n- 1) with k = n,at each iteration the
+   * array is partitioned into the **partial permutation and remaining values**. Although
+   * the subset that is returned is unique (it will be {0,1,...n-1}), all n! possible orderings
+   * of the elements in the set occur with equal probability.
+   * @param n
+   * @return
+   */
+  public static List<Integer> solution(int n) {
+    List<Integer> list = new ArrayList<>(n);
+    for (int i = 0; i < n; i++) {
+      list.add(i);
+    }
+
+    /*
+    实现.
+    Random random = new Random(31);
+    for (int i = 0; i < k; i++) {
+      // random.nextInt(given.size() - i) + i就是 [i, size - 1]
+      Collections.swap(given, i, random.nextInt(given.size() - i) + i);
+    }
+    return given.subList(0, k);
+     */
+    List<Integer> result = P11_SampleOfflineData.solution(list, list.size());
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+    List<Integer> result = solution(list.size());
+    System.out.println(result);
+
+  }
+
+}
